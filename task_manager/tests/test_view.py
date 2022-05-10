24a6f30913b9b741aa1623b79091e28e46e54988
emp7yhead <artyomkropp@gmail.com>
2022-05-10 21:50:27 +0900

    Test: view

--- task_manager/tests/test_view.py
+++ task_manager/tests/test_view.py
@@ -2,7 +2,12 @@ from django.test import TestCase
 
 
 class IndexPageViewTest(TestCase):
+    """Test for index page."""
+
+    status_code = 200
+
     def test_page(self):
+        """Tests page that the response to GET is valid."""
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, self.status_code)
         self.assertContains(response, "Task Manager")
