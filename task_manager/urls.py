11f31447ea701d499580190c7d025368e42f0bff
emp7yhead <artyomkropp@gmail.com>
2022-05-26 16:57:00 +0900

    Update: urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,9 +3,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from task_manager.views import IndexView
+from task_manager import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view()),
+    path('', views.IndexView.as_view(), name='index'),
+    path('users/', views.UserListView.as_view(), name='users'),
+    path('users/create/', views.RegisterUserView.as_view(), name='sign_up'),
+    # path('/users/<int:pk>/update/', views, name='update'),
+    # path('/users/<int:pk>/delete/', views, name='delete'),
+    # path('login/', views, name='login')
 ]
