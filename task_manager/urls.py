e4a840047a67dcd6ed86f617a0c16297bd65ffb3
emp7yhead <artyomkropp@gmail.com>
2022-05-29 23:59:55 +0900

    Update: urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,4 +1,4 @@
-"""task_manager URL Configuration."""
+"""task_manager URL routes."""
 
 from django.contrib import admin
 from django.urls import path
@@ -6,11 +6,43 @@ from django.urls import path
 from task_manager import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.IndexView.as_view(), name='index'),
-    path('users/', views.UserListView.as_view(), name='users'),
-    path('users/create/', views.RegisterUserView.as_view(), name='sign_up'),
-    # path('/users/<int:pk>/update/', views, name='update'),
-    # path('/users/<int:pk>/delete/', views, name='delete'),
-    # path('login/', views, name='login')
+    path(
+        'admin/',
+        admin.site.urls,
+    ),
+    path(
+        '',
+        views.IndexView.as_view(),
+        name='index',
+    ),
+    path(
+        'users/',
+        views.UserListView.as_view(),
+        name='users',
+    ),
+    path(
+        'users/create/',
+        views.RegisterUserView.as_view(),
+        name='registration',
+    ),
+    path(
+        'users/<int:pk>/update',
+        views.UpdateUserView.as_view(),
+        name='update',
+    ),
+    path(
+        'users/<int:pk>/delete/',
+        views.DeleteUserView.as_view(),
+        name='delete',
+    ),
+    path(
+        'login/',
+        views.LoginUserView.as_view(),
+        name='login',
+    ),
+    path(
+        'logout',
+        views.LogoutUserView.as_view(),
+        name='logout',
+    ),
 ]
