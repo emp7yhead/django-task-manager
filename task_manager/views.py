22b13e3d2ac4c8c6c53f25a7cf2735c1e4200c4c
emp7yhead <artyomkropp@gmail.com>
2022-05-31 12:03:43 +0900

    Update: views

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,73 +1,39 @@
 """Views for task manager."""
-from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
-from django.views.generic import (
-    CreateView,
-    DeleteView,
-    ListView,
-    TemplateView,
-    UpdateView,
-)
-
-from task_manager.forms import RegisterAndUpdateUserForm
+from django.views import generic
 
 
-class IndexView(TemplateView):
+class IndexView(generic.TemplateView):
     """Define view for index page."""
 
-    template_name = 'index.html'
-    extra_context = {'title': _('Task Manager')}
-
-
-class UserListView(ListView):
-    """Define view for users list page."""
-
-    model = User
-    template_name = 'users.html'
-    context_object_name = 'users'
-    extra_context = {'title': _('Users list')}
-
-
-class RegisterUserView(CreateView):
-    """Define view for registration page."""
-
-    form_class = RegisterAndUpdateUserForm
-    template_name = 'registration/register.html'
-    success_url = reverse_lazy('login')
-    extra_context = {'title': _('Register user')}
-
-
-class UpdateUserView(UpdateView):
-    """Define view for update user page."""
-
-    model = User
-    form_class = RegisterAndUpdateUserForm
-    template_name = 'registration/register.html'
-    success_url = reverse_lazy('index')
-    extra_context = {'title': _('Update user')}
-
-
-class DeleteUserView(DeleteView):
-    """Define view for delete user page."""
-
-    model = User
-    template_name = 'users/delete.html'
-    success_url = reverse_lazy('index')
-    extra_context = {'title': _('Delete user')}
+    template_name = "index.html"
+    extra_context = {"title": _("Task Manager")}
 
 
 class LoginUserView(SuccessMessageMixin, LoginView):
     """Define view for login page."""
 
-    template_name = 'registration/login.html'
-    next_page = 'index'
-    extra_context = {'title': _('Login')}
+    form_class = AuthenticationForm
+    template_name = "login.html"
+    extra_context = {"title": _("Login")}
+    success_message = "You're logged in"
+
+    def get_success_url(self):
+        """Change redirect url."""
+        return reverse_lazy("index")
 
 
-class LogoutUserView(LogoutView):
+class LogoutUserView(SuccessMessageMixin, LogoutView):
     """Define view for logout."""
 
-    next_page = 'index'
+    next_page = "index"
+
+    def dispatch(self, request, *args, **kwargs):
+        """Log out and show a logout message."""
+        messages.add_message(request, messages.INFO, "You're logged out.")
+        return super().dispatch(request, *args, **kwargs)
