e10357a78a3ad6f21c0760d9698b975a7b4839d2
emp7yhead <artyomkropp@gmail.com>
2022-05-31 12:03:51 +0900

    Update: urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,7 @@
 """task_manager URL routes."""
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from task_manager import views
 
@@ -16,33 +16,17 @@ urlpatterns = [
         name='index',
     ),
     path(
-        'users/',
-        views.UserListView.as_view(),
-        name='users',
-    ),
-    path(
-        'users/create/',
-        views.RegisterUserView.as_view(),
-        name='registration',
-    ),
-    path(
-        'users/<int:pk>/update',
-        views.UpdateUserView.as_view(),
-        name='update',
-    ),
-    path(
-        'users/<int:pk>/delete/',
-        views.DeleteUserView.as_view(),
-        name='delete',
-    ),
-    path(
         'login/',
         views.LoginUserView.as_view(),
         name='login',
     ),
     path(
-        'logout',
+        'logout/',
         views.LogoutUserView.as_view(),
         name='logout',
+    ),
+    path(
+        'users/',
+        include('task_manager.users.urls'),
     ),
 ]
