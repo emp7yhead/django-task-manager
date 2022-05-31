7e78467a3ecc13497299a30fa4888a7561e500c2
emp7yhead <artyomkropp@gmail.com>
2022-05-31 22:12:14 +0900

    Add: status model

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+MAX_LENGTH = 40
+
+
+class Status(models.Model):
+    """Model representing statuses."""
+
+    name = models.CharField(_('name'), max_length=MAX_LENGTH)
+    created_at = models.DateTimeField(_('creation date'), auto_now_add=True)
+
+    class Meta:
+        verbose_name = _('status')
+        verbose_name_plural = _('statuses')
+
+    def __str__(self):
+        """Represent model object."""
+        return self.name
