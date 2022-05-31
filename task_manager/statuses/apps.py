2f791aa34dd9e468ba7c713b870b4949b41e73c5
emp7yhead <artyomkropp@gmail.com>
2022-05-31 22:12:57 +0900

    Fix: add docstring in apps.py

--- task_manager/statuses/apps.py
+++ task_manager/statuses/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class StatusesConfig(AppConfig):
+    """Config for Statuses app."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'task_manager.statuses'
