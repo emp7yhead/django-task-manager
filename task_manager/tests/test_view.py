b7951f726330cef8e467b660245e52e888765ade
emp7yhead <artyomkropp@gmail.com>
2022-05-31 13:25:34 +0900

    Add: test for login view

--- task_manager/tests/test_view.py
+++ task_manager/tests/test_view.py
@@ -1,7 +1,9 @@
 """Test for index page view."""
 from http import HTTPStatus
 
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse_lazy
 
 
 class IndexPageViewTest(TestCase):
@@ -11,3 +13,64 @@ class IndexPageViewTest(TestCase):
         """Tests page that the response to GET is valid."""
         response = self.client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+class LoginPageViewTest(TestCase):
+    """Test user signin."""
+
+    fake_user = {
+        'username': 'test',
+        'password': 'random_password1',
+    }
+
+    def setUp(self):
+        """Create test user."""
+        self.user = User.objects.create_user(
+            username=self.fake_user['username'],
+            password=self.fake_user['password'],
+        )
+        self.user.save()
+
+    def tearDown(self):
+        """Delete test user."""
+        self.user.delete()
+
+    def test(self):
+        """Send registration request."""
+        response = self.client.get(
+            reverse_lazy('login'),
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self._test_correct()
+        self._test_wrong_password()
+        self._test_wrong_username()
+
+    def _test_correct(self):
+        response = self.client.post(
+            '/login/',
+            {
+                'username': self.fake_user['username'],
+                'password': self.fake_user['password'],
+            },
+        )
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+    def _test_wrong_username(self):
+        response = self.client.post(
+            '/login/',
+            {
+                'username': 'wrong',
+                'password': self.fake_user['password'],
+            },
+        )
+        self.assertFalse(response.status_code == HTTPStatus.FOUND)
+
+    def _test_wrong_password(self):
+        response = self.client.post(
+            '/login/',
+            {
+                'username': self.fake_user['username'],
+                'password': 'wrong',
+            },
+        )
+        self.assertFalse(response.status_code == HTTPStatus.FOUND)
