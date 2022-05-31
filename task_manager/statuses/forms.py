08d054e5e119e40b230395e8bf057e20e59d25b4
emp7yhead <artyomkropp@gmail.com>
2022-05-31 22:13:24 +0900

    Add: status form

---
+++ task_manager/statuses/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from .models import Status
+
+
+class CreateAndUpdateStatusForm(ModelForm):
+    """Form for creating and updating status."""
+
+    class Meta:
+        model = Status
+        fields = ('name',)
