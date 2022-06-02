f4671adb13efa53dd50df440dbaccff0604b3c62
emp7yhead <artyomkropp@gmail.com>
2022-06-02 22:28:56 +0900

    Fix: typo

--- task_manager/views.py
+++ task_manager/views.py
@@ -11,27 +11,27 @@ from django.views import generic
 class IndexView(generic.TemplateView):
     """Define view for index page."""
 
-    template_name = "index.html"
-    extra_context = {"title": _("Task Manager")}
+    template_name = 'index.html'
+    extra_context = {'title': _('Task Manager')}
 
 
 class LoginUserView(SuccessMessageMixin, LoginView):
     """Define view for login page."""
 
     form_class = AuthenticationForm
-    template_name = "login.html"
-    extra_context = {"title": _("Login")}
+    template_name = 'login.html'
+    extra_context = {'title': _('Login')}
     success_message = _("You're logged in")
 
     def get_success_url(self):
         """Change redirect url."""
-        return reverse_lazy("index")
+        return reverse_lazy('index')
 
 
 class LogoutUserView(SuccessMessageMixin, LogoutView):
     """Define view for logout."""
 
-    next_page = "index"
+    next_page = 'index'
 
     def dispatch(self, request, *args, **kwargs):
         """Log out and show a logout message."""
