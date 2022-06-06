c006cd468ba3a9e548c890f19a77d5d8ff51cf9a
emp7yhead <artyomkropp@gmail.com>
2022-06-06 23:45:21 +0900

    Fix: typo

--- task_manager/tasks/apps.py
+++ task_manager/tasks/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class TasksConfig(AppConfig):
     """Config for Statuses app."""
-    
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'task_manager.tasks'
