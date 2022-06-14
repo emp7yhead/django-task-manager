982503577b636147a5862d466fa0f8200d6a1d97
emp7yhead <artyomkropp@gmail.com>
2022-06-15 01:24:49 +0900

    Update: user model and views

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """Define custom User model."""
 
-    pass
+    def __str__(self):
+        """Represent model object."""
+        return self.get_full_name()
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,9 @@
 """Views for task manager users."""
+from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models import ProtectedError
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 from django.views import generic
@@ -45,7 +48,22 @@ class DeleteUserView(UserCheckMixin, generic.DeleteView):
     """Define view for delete user page."""
 
     model = User
-    template_name = "users/delete.html"
+    template_name = "delete.html"
     success_url = reverse_lazy("users:users")
     extra_context = {"title": _("Delete user")}
-    success_message = _("Successfully deleted user.")
+
+    def form_valid(self, form):
+        """Check if user has assigned task."""
+        try:
+            self.object.delete()
+        except ProtectedError:
+            messages.error(
+                self.request,
+                _('User has assigned task'),
+            )
+        else:
+            messages.success(
+                self.request,
+                _("Successfully deleted user."),
+            )
+        return HttpResponseRedirect(self.success_url)
