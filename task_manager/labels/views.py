b5801103aabc8ca15b754278691eedff57f07a81
emp7yhead <artyomkropp@gmail.com>
2022-07-01 00:06:00 +0900

    Add: view for labels app

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+"""View for Labels app."""
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
+from django.views import generic
+
+from task_manager.labels.forms import CreateAndUpdateLabelForm
+from task_manager.labels.models import Label
+
+
+class LabelsListView(LoginRequiredMixin, generic.ListView):
+    """Define view for labels list page."""
+
+    model = Label
+    template_name = 'labels/labels_list.html'
+    context_object_name = 'labels'
+
+    def get_context_data(self, **kwargs):
+        """Define the title text."""
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('Labels list')
+        return context
+
+
+class CreateLabelView(
+    LoginRequiredMixin,
+    SuccessMessageMixin,
+    generic.CreateView,
+):
+    """Define view for create label page."""
+
+    model = Label
+    form_class = CreateAndUpdateLabelForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('labels:labels')
+    success_message = _('Label successfully created.')
+
+    def get_context_data(self, **kwargs):
+        """Define the title and button text."""
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('Create label')
+        context['button'] = _('Create')
+        return context
+
+
+class UpdateLabelView(
+    LoginRequiredMixin,
+    SuccessMessageMixin,
+    generic.UpdateView,
+):
+    """Define view for update label page."""
+
+    model = Label
+    form_class = CreateAndUpdateLabelForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('labels:labels')
+    success_message = _('Label successfully updated.')
+
+    def get_context_data(self, **kwargs):
+        """Define the title and button text."""
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('Update label')
+        context['button'] = _('Update')
+        return context
+
+
+class DeleteLabelView(
+    LoginRequiredMixin,
+    SuccessMessageMixin,
+    generic.DeleteView,
+):
+    """Define view for delete label page."""
+
+    model = Label
+    template_name = 'delete.html'
+    success_url = reverse_lazy('labels:labels')
+    success_message = _('Label successfully deleted.')
+
+    def get_context_data(self, **kwargs):
+        """Define the title text."""
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('Delete label')
+        return context
