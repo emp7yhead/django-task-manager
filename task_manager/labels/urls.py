1165bd72a518b154a8d6be2a22758138bb03885a
emp7yhead <artyomkropp@gmail.com>
2022-07-01 00:05:47 +0900

    Add: url routes for label app

---
+++ task_manager/labels/urls.py
@@ -0,0 +1,28 @@
+"""task_manager.tasks URL routes."""
+from django.urls import path
+
+from task_manager.labels import views
+
+app_name = 'labels'
+urlpatterns = [
+    path(
+        '',
+        views.LabelsListView.as_view(),
+        name='labels',
+    ),
+    path(
+        'create/',
+        views.CreateLabelView.as_view(),
+        name='create',
+    ),
+    path(
+        '<int:pk>/update',
+        views.UpdateLabelView.as_view(),
+        name='update',
+    ),
+    path(
+        '<int:pk>/delete/',
+        views.DeleteLabelView.as_view(),
+        name='delete',
+    ),
+]
