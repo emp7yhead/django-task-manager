b5651895771130aa803347348ecc07c1d3a67fbf
emp7yhead <artyomkropp@gmail.com>
2022-07-11 00:18:39 +0900

    Add: labels test

---
+++ task_manager/tests/test_labels.py
@@ -0,0 +1,77 @@
+"""Tests for labels app."""
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse_lazy
+from faker import Faker
+
+from task_manager.labels.models import Label
+
+User = get_user_model()
+
+
+class LabelsTest(TestCase):
+    """Test user login."""
+
+    def setUp(self):
+        """Create test user."""
+        self.faker = Faker()
+        self.username = self.faker.user_name()
+        self.password = self.faker.password(length=10)
+        self.user = User.objects.create_user(
+            username=self.username,
+            password=self.password,
+        )
+        self.user.save()
+        self.name = self.faker.pystr()
+        self.label = Label.objects.create(
+            name=self.name,
+        )
+        self.label.save()
+
+    def tearDown(self):
+        """Teardown database."""
+        self.user.delete()
+        self.label.delete()
+
+    def test(self):
+        """Tests for labels."""
+        self.client.force_login(self.user)
+        response = self.client.get(
+            reverse_lazy('labels:labels'),
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self._test_create_label()
+        self._test_update_label(self.label)
+        self._test_delete_label(self.label)
+
+    def _test_create_label(self):
+        response = self.client.post(
+            reverse_lazy('labels:create'),
+            {'name': 'test label'},
+        )
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+    def _test_update_label(self, label):
+        response = self.client.post(
+            reverse_lazy(
+                'labels:update',
+                args=(label.id,),
+            ),
+            {'name': 'test'},
+        )
+        changed_label = Label.objects.get(name='test')
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(label.id, changed_label.id)
+
+    def _test_delete_label(self, label):
+        response = self.client.post(
+            reverse_lazy(
+                'labels:delete',
+                args=(label.id,),
+            ),
+        )
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        with self.assertRaises(Label.DoesNotExist):
+            Label.objects.get(pk=label.id)
