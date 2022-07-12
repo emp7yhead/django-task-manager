7e075bb9b6ab6f3a942258a3e46b92e0edcda422
emp7yhead <artyomkropp@gmail.com>
2022-07-12 17:25:59 +0900

    Remove: templatetags

--- task_manager/templatetags/custom_filter.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def get_verbose_name(model):
-    """Return model verbose name."""
-    return model._meta.verbose_name  # noqa: WPS437
