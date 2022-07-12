11cf4f24dd54b324f373fe5bc4b109a725d1cb5e
emp7yhead <artyomkropp@gmail.com>
2022-07-12 18:15:08 +0900

    Fix: filters translation

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,10 +1,13 @@
 """Filters for task app."""
 from django import forms
 from django.contrib.auth import get_user_model
+from django.db.models import Value
+from django.db.models.functions import Concat
 from django.utils.translation import gettext as _
 from django_filters import BooleanFilter, ChoiceFilter, FilterSet
 
 from task_manager.labels.models import Label
+from task_manager.statuses.models import Status
 from task_manager.tasks.models import Task
 
 User = get_user_model()
@@ -12,6 +15,14 @@ User = get_user_model()
 
 class TaskFilter(FilterSet):
     """Define filers for tasks list."""
+
+    statuses_query = Status.objects.values_list('id', 'name')
+    statuses = ChoiceFilter(label=_('Status'), choices=statuses_query)
+
+    executors_query = User.objects.values_list(
+        'id', Concat('first_name', Value(' '), 'last_name'),
+    )
+    executors = ChoiceFilter(label=_('Executor'), choices=executors_query)
 
     labels_query = Label.objects.values_list('id', 'name')
     labels = ChoiceFilter(label=_('Label'), choices=labels_query)
@@ -31,7 +42,7 @@ class TaskFilter(FilterSet):
     class Meta:
         model = Task
         fields = [
-            'status',
-            'executor',
+            'statuses',
+            'executors',
             'labels',
         ]
